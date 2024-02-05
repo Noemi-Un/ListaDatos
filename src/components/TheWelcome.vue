574d831957ed15ec4c00a0fb02a7cf9d2302ed79
Noemi-Un <124269534+Noemi-Un@users.noreply.github.com>
2024-02-05 06:20:41 -0500

    Add files via upload

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>Bienvenido a mi sitio c:</h1>
+    <p>Este es un mensaje de bienvenida.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped></style>
